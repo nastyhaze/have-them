1681363bb7cb1a67ebba5969fc167f25548b47a6
danla <dan.lawrence.walker@gmail.com>
2022-04-06 15:50:41 -0700

    Initial commit

---
+++ src/main/java/com/nastyHaze/havethem/HaveThemApplication.java
@@ -0,0 +1,13 @@
+package com.nastyHaze.havethem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveThemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveThemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nastyHaze/havethem/HaveThemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nastyHaze.havethem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveThemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
